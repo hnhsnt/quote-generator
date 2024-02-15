d45be44796a7542a3162c5c691cc2b4e58d54552
hnhsnt <133853548+hnhsnt@users.noreply.github.com>
2024-02-15 21:13:13 +0100

    added event listeners to the two buttons and their respective functions

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace it with 'unknown'
+    if (!quote.author) {
+        authorText.textContent = 'unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote'); // we'll add this class that's been manipulated in CSS
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
 }
 
-// Get quotes from API using an asynchronous fetch request within a try-catch statement
-// An asynchronous function can run at any time independently and it won't stop the browser from completing the loading of a page
-// async function getQuotes() {
-//     const apiUrl = 'https://type.fit/api/quotes';
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
 
-//     try { // allows us to attempt/complete a fetch a request
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     } catch (error) { // we can catch our error information and do something with it
-//         // handle the errors here
-//     }
-// }
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
-// On Load
-// getQuotes();
 newQuote();
