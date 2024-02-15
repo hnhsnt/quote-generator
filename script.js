1617bd282d331deca74531c21f25207bcc177d87
hnhsnt <133853548+hnhsnt@users.noreply.github.com>
2024-02-15 21:38:29 +0100

    added a loading icon

--- script.js
+++ script.js
@@ -3,26 +3,45 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false; // we don't want it to be hidden;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 // Show New Quote
 function newQuote() {
-    // Pick a random quote from apiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    // Check if author field is blank and replace it with 'unknown'
-    if (!quote.author) {
-        authorText.textContent = 'unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
+    loading();
 
-    // Check quote length to determine styling
-    if (quote.text.length > 120) {
-        quoteText.classList.add('long-quote'); // we'll add this class that's been manipulated in CSS
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
+    setTimeout(() => {
+        // Pick a random quote from apiQuotes array
+        const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+        // Check if author field is blank and replace it with 'unknown'
+        if (!quote.author) {
+            authorText.textContent = 'unknown';
+        } else {
+            authorText.textContent = quote.author;
+        }
 
-    quoteText.textContent = quote.text;
+        // Check quote length to determine styling
+        if (quote.text.length > 120) {
+            quoteText.classList.add('long-quote'); // we'll add this class that's been manipulated in CSS
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+
+        // Set quote, hide loader
+        quoteText.textContent = quote.text;
+        complete();
+    }, 700);
 }
 
 // Tweet Quote
